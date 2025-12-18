9e274d6dac1c29262ddeaa4dc7b5fa7eff400323
Brian Holt <bholt@dwolla.com>
2025-12-18 15:55:49 -0600

    add serviceName field to ConsulDiscoverable typeclass

--- smithy4s-tests/src/test/scala-2/com/dwolla/consul/smithy4s/ConsulDiscoverableSpecPerScalaVersion.scala
+++ smithy4s-tests/src/test/scala-2/com/dwolla/consul/smithy4s/ConsulDiscoverableSpecPerScalaVersion.scala
@@ -1,5 +1,6 @@
 package some_other_package
 
+import com.dwolla.consul.ServiceName
 import com.dwolla.consul.smithy4s._
 import com.dwolla.test.HelloService
 import munit.FunSuite
@@ -10,11 +11,13 @@ trait ConsulDiscoverableSpecPerScalaVersion { self: FunSuite =>
   val serviceUri: Uri = UriFromService(HelloService)
   private val serviceHost: Host = HostFromService(HelloService)
   private val serviceUriAuthority: Uri.Authority = UriAuthorityFromService(HelloService)
+  private val serviceName: ServiceName = ServiceNameFromService(HelloService)
 
   test("ConsulDiscoverable typeclass macro constructs a working instance of the typeclass") {
     assertEquals(ConsulDiscoverable[HelloService].host, serviceHost)
     assertEquals(ConsulDiscoverable[HelloService].uriAuthority, serviceUriAuthority)
     assertEquals(ConsulDiscoverable[HelloService].uri, serviceUri)
+    assertEquals(ConsulDiscoverable[HelloService].serviceName, serviceName)
   }
 
   test("ConsulDiscoverable typeclass macro returns no instance when the type parameter isn't a Smithy Service") {
--- smithy4s/src/main/scala/com/dwolla/consul/smithy4s/ConsulDiscoverable.scala
+++ smithy4s/src/main/scala/com/dwolla/consul/smithy4s/ConsulDiscoverable.scala
@@ -1,17 +1,27 @@
 package com.dwolla.consul.smithy4s
 
+import cats.syntax.all._
+import com.dwolla.consul.ServiceName
 import org.http4s.Uri
-import org.http4s.Uri.Host
+import org.http4s.syntax.all._
 
 import scala.annotation.implicitNotFound
 
 @implicitNotFound("Instances are only available for Smithy4s Services annotated with @discoverable")
-trait ConsulDiscoverable[Alg[_[_]]] {
-  def host: Host
+sealed trait ConsulDiscoverable[Alg[_[_]]] {
+  def host: Uri.Host
   def uriAuthority: Uri.Authority
   def uri: Uri
+  def serviceName: ServiceName = ServiceName(host.value) // default implementation for MiMa compatibility, but it's never used in practice
 }
 
 object ConsulDiscoverable extends ConsulDiscoverablePlatform {
   def apply[Alg[_[_]] : ConsulDiscoverable]: ConsulDiscoverable[Alg] = implicitly
+
+  def make[Alg[_[_]]](name: ServiceName, dest: Uri.Host): ConsulDiscoverable[Alg] = new ConsulDiscoverable[Alg] {
+    override def host: Uri.Host = dest
+    override def uriAuthority: Uri.Authority = Uri.Authority(host = host)
+    override def uri: Uri = Uri(scheme = scheme"consul".some, authority = uriAuthority.some)
+    override def serviceName: ServiceName = name
+  }
 }
